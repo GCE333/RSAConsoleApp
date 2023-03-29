a8029724d1f8ddf19b5ae2191b95cf3a81a0beb6
George Economakis <economakis.george@gmail.com>
2023-03-29 15:00:21 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RSAConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
